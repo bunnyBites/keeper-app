de92b3918e72af85b2be95f50bf6c595775d6c54
bunnyBites <s.mukeshkr481@gmail.com>
2023-04-03 09:49:24 +0530

    add create area component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
+import CreateArea from "./components/CreateArea";
 import Note from "./components/note";
 import { notes } from "./constants/notes";
 import "./App.css";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((note) => (
         <Note {...note} />
       ))}
