dcdedeb74ba2e011aca762c7d19a5dbc57734dab
bunnyBites <s.mukeshkr481@gmail.com>
2023-04-03 10:03:25 +0530

    add funtionality to create a note

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 import Header from "./components/header";
 import Footer from "./components/footer";
 import CreateArea from "./components/CreateArea";
 import Note from "./components/note";
-import { notes } from "./constants/notes";
 import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea setNotes={setNotes} />
       {notes.map((note) => (
         <Note {...note} />
       ))}
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,13 +1,44 @@
-const CreateArea = () => {
+import { useState } from "react";
+
+const CreateArea = ({ setNotes }) => {
+  const [noteInfo, setNoteInfo] = useState({
+    title: "",
+    content: "",
+  });
+
+  const handleSubmit = () => {
+    setNotes(prevValue => [...prevValue, { ...noteInfo, key: prevValue.length }]);
+    setNoteInfo({ title: "", content: "" });
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setNoteInfo(prevValue => ({ ...prevValue, [name]: value }))
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={noteInfo.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={noteInfo.content}
+        />
+        <button type="button" onClick={handleSubmit}>
+          Add
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default CreateArea;
